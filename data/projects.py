84d884d13c79f31bed6228ce918712db721c264a
DonthuMukeshGuptha <130040567+DonthuMukeshGuptha@users.noreply.github.com>
2025-07-02 12:16:46 +0530

    Initial commit: Flask portfolio

---
+++ data/projects.py
@@ -0,0 +1,14 @@
+projects = [
+    {
+        'title': 'Smart Resume Analyzer',
+        'description': 'A Flask app that parses resumes, matches skills to jobs, and gives score-based recommendations.',
+        'github': 'https://github.com/Harshavardhandonthu/Visualizing-Housing-Market-Trends-An-Analysis-of-Sale-Prices-and-Features-using-Tableau',
+        'demo': ''
+    },
+    {
+        'title': 'Housing Market Visualizer',
+        'description': 'A Tableau-powered data visualization project on house sale prices and trends.',
+        'github': 'https://github.com/Harshavardhandonthu/smart-resume-analyzer',
+        'demo': 'https://drive.google.com/file/d/1jWU9bB2cKyY9fbzkhcaM4iiKVDI3asuw/view?usp=drive_link'
+    }
+]
