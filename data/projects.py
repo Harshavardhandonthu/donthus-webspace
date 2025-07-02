fd64813680dabc36d7f92f7b58a7713bee054c47
DonthuMukeshGuptha <130040567+DonthuMukeshGuptha@users.noreply.github.com>
2025-07-02 14:23:26 +0530

    Added resume download section

--- data/projects.py
+++ data/projects.py
@@ -2,13 +2,13 @@ projects = [
     {
         'title': 'Smart Resume Analyzer',
         'description': 'A Flask app that parses resumes, matches skills to jobs, and gives score-based recommendations.',
-        'github': 'https://github.com/Harshavardhandonthu/Visualizing-Housing-Market-Trends-An-Analysis-of-Sale-Prices-and-Features-using-Tableau',
+        'github': 'https://github.com/Harshavardhandonthu/smart-resume-analyzer',
         'demo': ''
     },
     {
         'title': 'Housing Market Visualizer',
         'description': 'A Tableau-powered data visualization project on house sale prices and trends.',
-        'github': 'https://github.com/Harshavardhandonthu/smart-resume-analyzer',
+        'github': 'https://github.com/Harshavardhandonthu/Visualizing-Housing-Market-Trends-An-Analysis-of-Sale-Prices-and-Features-using-Tableau',
         'demo': 'https://drive.google.com/file/d/1jWU9bB2cKyY9fbzkhcaM4iiKVDI3asuw/view?usp=drive_link'
     }
 ]
